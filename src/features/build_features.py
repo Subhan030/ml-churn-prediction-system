31071033162233cf78fa5f41bd27fb7f34cb4405
Subhan030 <raisubhan728@gmail.com>
2026-02-01 17:48:36 +0530

    Fixed refactoring Error

--- src/features/build_features.py
+++ src/features/build_features.py
@@ -3,7 +3,7 @@ from sklearn.compose import ColumnTransformer
 from sklearn.pipeline import Pipeline
 
 def get_feature_lists():
-    numerical_features = ["tenure","MonthlyCharges","TotalCharges"]
+    numerical_features = ["tenure","MonthlyCharges","TotalCharges","SeniorCitizen"]
     categorical_features = ["Contract","PaymentMethod","InternetService"]
     binary_features = ["Partner","Dependents","PaperlessBilling"]
     return numerical_features, categorical_features, binary_features
@@ -16,12 +16,15 @@ def build_preprocessor(numerical_features, categorical_features, binary_features
     categorical_transformer = Pipeline(
         steps=[("onehot", OneHotEncoder(handle_unknown="ignore"))]
     )
+    binary_transformer = Pipeline(
+        steps=[("onehot", OneHotEncoder(drop="if_binary"))]
+    )
 
     preprocessor = ColumnTransformer(
         transformers=[
             ("num", numeric_transformer, numerical_features),
             ("cat", categorical_transformer, categorical_features),
-            ("bin", "passthrough", binary_features),
+            ("bin", binary_transformer, binary_features),
         ]
     )
 
